272ec16ef73aacfb13d47cbdfa0c0ac62fedbfc2
finder1793 <kangas.kyle@gmail.com>
2024-12-07 16:15:49 -0600

    Gradle makes me cry as does maven Jenkins Doesnt like me this is my life

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 rootProject.name = "BreweryX"
 plugins {
-    id 'org.gradle.toolchains.foojay-resolver-convention' version '0.5.0'
+    id 'org.gradle.toolchains.foojay-resolver-convention' version '0.5.0';
 }
